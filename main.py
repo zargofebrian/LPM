d9601a4d647bc560733f4d7a7eba1c8a1f2628a7
zargofebrian <145729410+zargofebrian@users.noreply.github.com>
2024-05-31 12:05:04 +0700

    main.py

--- main.py
+++ main.py
@@ -4,22 +4,32 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import Message
 
 PROXY_URL = 'http://proxy.server:3128'
-TOKEN = '7190621333:AAGk6ReUvuWbDN2XNOpkdAMiEOAebnIp2Lg'
 
-bot = Bot(token=TOKEN)
+bot = Bot(token="6945981247:AAHuPO0UgNv2z0j3HyW5EpDzFQBurLBFw-w", proxy=PROXY_URL)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-CHANNEL_ID = -1002019991143
+CHANNEL_ID = -1002079102928
 
 @dp.message_handler(commands=['start'])
 async def handler_start(message: types.Message):
-    quote_text = "haloo 👋🏻, bagi kalian yang ingin mengirim pm gunakan :  <blockquote>#boy / #girl</blockquote> kamu juga bisa menggunakan <a href='https://t.me/roleplay_base/665'>AcceptedHashtag</a> jangan lupa taruh username kamu yaa ❕❕"
+    quote_text = "halooo, gimana nih kabarnya ❔ pasti kamu mau kirim menfess ya. <blockquote>Sebelum kirim menfess jangan lupa perhatikan menfes kalian ya apakah sudah sesuai dengan ht dan tidak melanggar rulesnya</blockquote> untuk mengetahui apakah ht kalian sesuai silahkan klik 📂 /hashtag"
     await message.reply(text=quote_text, parse_mode=ParseMode.HTML)
+
+@dp.message_handler(commands=['hashtag'])
+async def send_all_hashtags(message: types.Message):
+    response = (
+        "Berikut adalah penjelasan untuk semua hashtag di channel kami:\n\n"
+        "#ELF = untuk bertanya kepada dreamies dan temukan apa yang kalian cari sebagai jawaban di dunia roleplayer ini.\n\n"
+        "#FAIRY =  untuk menceritakan pengalaman kalian kepada dreamies tentang dunia roleplayer ini.\n\n"
+        "#MERMAID = untuk mencari seseorang yang telah lama di cari seperti teman, pasangan, mutualan channel, akun, dan circle atau paguy..\n\n"
+    )
+    await message.reply(response)
+    await message.answer_sticker('CAACAgUAAxkBAAEL_C9mKf2y-q1CD2_wOxneHKQd5ZUupQACdwwAAuajUFXgkBo0l99wcTQE')
 
 
 from database import *
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=False
+    executor.start_polling(dp, skip_updates=True)
